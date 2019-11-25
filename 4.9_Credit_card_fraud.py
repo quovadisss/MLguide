89f7255fa87685137e13da15df99a6aef4216a25
quovadisss <shadows92@naver.com>
2019-11-26 00:10:54 +0900

    2

--- 4.9_Credit_card_fraud.py
+++ 4.9_Credit_card_fraud.py
@@ -2,8 +2,36 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import warnings
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 warnings.filterwarnings("ignore")
+
 # %matplotlib inline
 
-card_df = pd.read_csv("/MLguide/creditcard.csv")
-card_df.head()
+card_df = pd.read_csv("creditcard.csv")
+print(card_df.head())
+
+# 인자로 입력받은 DataFrame을 복사한 뒤 Time 칼럼만 삭제하고 복사된 DataFrame 반환
+def get_preprocessed_df(df=None):
+    df_copy = df.copy()
+    df_copy.drop('Time', axis=1, inplace=True)
+    return df_copy
+
+# 사전 데이터 가공 후 학습과 데스트 데이터 세트를 반환하는 함수.
+def get_train_test_dataset(df=None):
+    # 인자로 입력된 DataFrame의 사전 데이터 사공이 완료된 복사 DataFrame 반환
+    df_copy = get_preprocessed_df(df)
+    # DataFrame의 맨 마지막 칼럼이 레이블, 나머지는 피처들
+    X_features = df_copy.iloc[:, :-1]
+    y_target = df_copy.iloc[:, -1]
+    # train_test_split()으로 학습과 테스트 데이터 분할. stratify=y_target으로 Stratified 기반 분할
+    X_train, X_test, y_train, y_test = train_test_split(X_features, y_target, test_size=0.3, random_state=0, stratify=y_target)
+    return X_train, X_test, y_train, y_test
+
+X_train, X_test, y_train, y_test = get_train_test_dataset(card_df)
+
+lr_clf = LogisticRegression()
+lr_clf.fit(X_train, y_train)
+lr_pred = lr_clf.predict(X_test)
+
+# 3장에서 사용한 get_clf_eval()함수를 이용해 평가 수행.
