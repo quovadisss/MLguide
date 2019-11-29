b74c17379a820c5f9da55a4d9696e605aa9ee5da
quovadisss <shadows92@naver.com>
2019-11-29 23:05:34 +0900

    1

--- 4.9_Credit_card_fraud.py
+++ 4.9_Credit_card_fraud.py
@@ -34,4 +34,141 @@ lr_clf = LogisticRegression()
 lr_clf.fit(X_train, y_train)
 lr_pred = lr_clf.predict(X_test)
 
-# 3장에서 사용한 get_clf_eval()함수를 이용해 평가 수행.
+# 3장에서 사용한 get_clf_eval()함수를 이용해 평가 수행
+def get_clf_eval(y_test , pred):
+    confusion = confusion_matrix(y_test, pred)
+    accuracy = accuracy_score(y_test, pred)
+    precision = precision_score(y_test, pred)
+    recall = recall_score(y_test, pred)
+    f1 = f1_score(y_test, pred)
+    # ROC-AUC 추가
+    roc_auc = roc_auc_score(y_test, pred)
+    print('오차 행렬')
+    print(confusion)
+    # ROC-AUC print 추가
+    print('정확도: {0:.4f}, 정밀도: {1:.4f}, 재현율: {2:.4f},\
+    F1: {3:.4f}, AUC:{4:.4f}'.format(accuracy, precision, recall, f1, roc_auc))
+
+
+get_clf_eval(y_test, lr_pred)
+
+# 인자로 사이킷런의 Estimator객체와 학습/테스트 데이터 세트를 입력 받아서 학습/예측/평가 수행.
+def get_model_train_eval(model, ftr_train=None, ftr_test=None, tgt_train=None, tgt_test=None):
+    model.fit(ftr_train, tgt_train)
+    pred = model.predict(ftr_test)
+    get_clf_eval(tgt_test, pred)
+
+lgbm_clf = LGBMClassifier(n_estimators=1000, num_leaves=64, n_jobs=1, boost_from_average=False)
+get_model_train_eval(lgbm_clf, ftr_train=X_train, ftr_test=X_test, tgt_train=y_train, tgt_test=y_test)
+
+
+# 데이터 분포도 변환 후 모델 학습/예측/평가
+plt.figure(figsize=(8,4))
+plt.xticks(range(0, 30000, 1000), rotation=60)
+sns.distplot(card_df['Amount'])
+plt.show()
+
+# 사이킷런의 StandardScaler를 이용해 정규 분포 형태로 Amount 피처값 변환하는 로직으로 수정.
+def get_preprocessed_df(df=None):
+    df_copy = df.copy()
+    scaler = StandardScaler()
+    amount_n = scaler.fit_transform(df_copy['Amount'].values.reshape(-1, 1))
+    # 변환된 Amount를 Amount_Scaled로 피처명 변경 후 DataFrame맨 앞 칼럼으로 입력
+    df_copy.insert(0, 'Amount_Scaled', amount_n)
+    # 기존 Time, Amount 피처 삭제
+    df_copy.drop(['Time', 'Amount'], axis=1, inplace=True)
+    return df_copy
+
+
+# Amount를 정규 분포 형태로 변환 후 로지스틱 회귀 및 lightGBM 수행.
+X_train, X_test, y_train, y_test = get_train_test_dataset(card_df)
+
+print('### 로지스틱 회귀 예측 성능 ###')
+lr_clf = LogisticRegression()
+get_model_train_eval(lr_clf, ftr_train=X_train, ftr_test=X_test, tgt_train=y_train,
+                     tgt_test=y_test)
+
+print('### LightGBM 예측 성능 ###')
+lgbm_clf = LGBMClassifier(n_estimators=1000, num_leaves=64, n_jobs=-1, boost_from_average=False)
+get_model_train_eval(lgbm_clf, ftr_train=X_train, ftr_test=X_test, tgt_train=y_train,
+                     tgt_test=y_test)
+
+# 로그 사용
+# : 데이터 분포도가 심하게 왜곡되어 있을 때 사용. 원래 값을 log 값으로 변환해 원래 큰 값을 상대적으로 작은 값으로 변환하기 때문에 왜곡을 상당 수준 개선해줌. numpy의 log1p() 함수 사용.
+def get_preprocessed_df(df=None):
+    df_copy = df.copy()
+    # 넘파이의 log1p( )를 이용하여 Amount를 로그 변환
+    amount_n = np.log1p(df_copy['Amount'])
+    df_copy.insert(0, 'Amount_Scaled', amount_n)
+    df_copy.drop(['Time','Amount'], axis=1, inplace=True)
+    return df_copy
+
+
+# 다시 로그 변환에 대한 예측 성능 평가
+X_train, X_test, y_train, y_test = get_train_test_dataset(card_df)
+
+print('### 로지스틱 회귀 예측 성능 ###')
+get_model_train_eval(lr_clf, ftr_train=X_train, ftr_test=X_test, tgt_train=y_train, tgt_test=y_test)
+
+print('### LightGBM 예측 성능 ###')
+get_model_train_eval(lgbm_clf, ftr_train=X_train, ftr_test=X_test, tgt_train=y_train, tgt_test=y_test)
+
+
+
+# 이상치 제거 후 평가
+import seaborn as sns
+plt.figure(figsize=(8,4))
+plt.xticks(range(0, 30000, 1000), rotation=60)
+sns.distplot(card_df['Amount'])
+
+import numpy as np
+
+def get_outlier(df=None, column=None, weight=1.5):
+    # fraud에 해당하는 column 데이터만 추출, 1/4 분위와 3/4 분위 지점을 np.percentile로 구함.
+    fraud = df[df['Class']==1][column]
+    quantile_25 = np.percentile(fraud.values, 25)
+    quantile_75 = np.percentile(fraud.values, 75)
+    # IQR을 구하고, IQR에 1.5를 곱하여 최대값과 최소값 지점 구함.
+    iqr = quantile_75 - quantile_25
+    iqr_weight = iqr * weight
+    lowest_val = quantile_25 - iqr_weight
+    highest_val = quantile_75 + iqr_weight
+    # 최대값 보다 크거나, 최소값 보다 작은 값을 아웃라이어로 설정하고 DataFrame index 반환.
+    outlier_index = fraud[(fraud < lowest_val) | (fraud > highest_val)].index
+    return outlier_index
+
+outlier_index = get_outlier(df=card_df, column='V14', weight=1.5)
+print('이상치 데이터 인덱스:', outlier_index)
+
+# get_processed_df( )를 로그 변환 후 V14 피처의 이상치 데이터를 삭제하는 로직으로 변경.
+def get_preprocessed_df(df=None):
+    df_copy = df.copy()
+    amount_n = np.log1p(df_copy['Amount'])
+    df_copy.insert(0, 'Amount_Scaled', amount_n)
+    df_copy.drop(['Time','Amount'], axis=1, inplace=True)
+    # 이상치 데이터 삭제하는 로직 추가
+    outlier_index = get_outlier(df=df_copy, column='V14', weight=1.5)
+    df_copy.drop(outlier_index, axis=0, inplace=True)
+    return df_copy
+
+X_train, X_test, y_train, y_test = get_train_test_dataset(card_df)
+print('### 로지스틱 회귀 예측 성능 ###')
+get_model_train_eval(lr_clf, ftr_train=X_train, ftr_test=X_test, tgt_train=y_train, tgt_test=y_test)
+print('### LightGBM 예측 성능 ###')
+get_model_train_eval(lgbm_clf, ftr_train=X_train, ftr_test=X_test, tgt_train=y_train, tgt_test=y_test)
+
+
+# SMOTE Over Sampling
+from imblearn.over_sampling import SMOTE
+
+smote = SMOTE(random_state=0)
+X_train_over, y_train_over = smote.fit_sample(X_train, y_train)
+print('SMOTE 적용 전 학습용 피처/레이블 데이터 세트: ', X_train.shape, y_train.shape)
+print('SMOTE 적용 후 학습용 피처/레이블 데이터 세트: ', X_train_over.shape, y_train_over.shape)
+print('SMOTE 적용 후 레이블 값 분포: \n', pd.Series(y_train_over).value_counts())
+
+lr_clf = LogisticRegression()
+# ftr_train과 tgt_train 인자값이 SMOTE 증식된 X_train_over와 y_train_over로 변경됨에 유의
+get_model_train_eval(lr_clf, ftr_train=X_train_over, ftr_test=X_test, tgt_train=y_train_over, tgt_test=y_test)
+
+# 결론 : 그래서 좋은 SMOTE 패키지 일수록 재현율 증가율은 높이고 정밀도 감소율은 줄인다.
